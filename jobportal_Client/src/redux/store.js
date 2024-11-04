babf7042cbdd3d2497bf06d2495e25c1c1b6a853
DangGiaThuan <danggiathuanhl@gmail.com>
2024-11-04 23:15:46 +0700

    update

--- jobportal_Client/src/redux/store.js
+++ jobportal_Client/src/redux/store.js
@@ -6,12 +6,14 @@ import { indutryReducer } from "./Industry/industry.reducer";
 import {forgotPasswordReducer} from "./ForgotPassword/forgotPassword.reducer"
 import { companyReducer } from "./Company/company.reducer";
 import { cityReducer } from "./City/city.reducer";
+import { applyJobReducer } from "./ApplyJob/applyJob.reducer";
 const rootReducer = combineReducers({
     auth:authReducer,
     jobPost:jobPostReducer,
     industry:indutryReducer,
     forgotPassword: forgotPasswordReducer,
     company:companyReducer,
-    city:cityReducer
+    city:cityReducer,
+    applyJob:applyJobReducer
 })
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
--- jobportal_Server/job-portal/src/main/java/com/job_portal/repository/ApplyJobRepository.java
+++ jobportal_Server/job-portal/src/main/java/com/job_portal/repository/ApplyJobRepository.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.job_portal.DTO.ApplyJobInProfile;
 import com.job_portal.models.ApplyJob;
 import com.job_portal.models.IdApplyJob;
 
@@ -21,4 +24,23 @@ public interface ApplyJobRepository extends JpaRepository<ApplyJob, IdApplyJob> 
 	           "WHERE a.isSave = true AND a.userId = :userId AND jp.company.companyId = :companyId")
 	    boolean existsByUserIdAndCompanyId(@Param("userId") UUID userId, 
 	                                        @Param("companyId") UUID companyId);
+	@Query("SELECT new com.job_portal.DTO.ApplyJobInProfile(" +
+	        "a.userId, " +
+	        "a.postId, " +
+	        "a.isSave, " +
+	        "a.applyDate, " +
+	        "a.pathCV, " +
+	        "jp.salary, " +
+	        "jp.location, " +
+	        "jp.title, " +
+	        "c.companyName, " +
+	        "jp.typeOfWork, " +
+	        "c.logo)" +
+	        "FROM ApplyJob a " +
+	        "JOIN a.jobPost jp " +
+	        "JOIN Seeker sp ON sp.userId = a.userId " +
+	        "JOIN Company c ON jp.company.companyId = c.companyId " +
+	        "WHERE sp.userId = :userId")
+	Page<ApplyJobInProfile> findApplyJobByUserId(@Param("userId") UUID userId, Pageable pageable);
+
 }
