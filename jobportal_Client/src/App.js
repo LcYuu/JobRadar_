0caade36a19402dc45346d3d65e4ef961e31f0a4
DangGiaThuan <danggiathuanhl@gmail.com>
2024-11-04 23:21:04 +0700

    a

--- jobportal_Client/src/App.js
+++ jobportal_Client/src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import './App.css';
 import Header from './components/common/Header/header';
 import Home from './pages/Home/Home';
@@ -9,27 +8,26 @@ import SignInForm from "./pages/SignIn/SignIn";
 import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
 import FindJobs from "./pages/FindJobs/FindJobs";
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import { getProfileAction } from "./redux/Auth/auth.action";
 import ChangePassword from "./pages/ForgotPassword/ChangePassword";
 import MyAccount from "./pages/MyAccount/MyAccount";
-import FindCompanies from "./pages/FindComapnies/FindCompanies";
-
 const App = () => {
+  const location = useLocation();
   const { auth } = useSelector(store => store);
   const dispatch = useDispatch();
+  const jwt = localStorage.getItem("jwt");
 
   useEffect(() => {
-    const jwt = sessionStorage.getItem("jwt");
     if (jwt) {
       dispatch(getProfileAction(jwt));
     }
-  }, [dispatch]);
+  }, [jwt, dispatch]);
 
   const isAuthenticated = !!auth.user;
 
-  // Kiểm tra đường dẫn hiện tại
-  const noHeaderPaths = ['/auth/sign-up', '/auth/sign-in', '/auth/forgot-password'];
-  const showHeader = !noHeaderPaths.includes(window.location.pathname);
+  // Ẩn Header nếu người dùng đang ở trang đăng ký và đăng nhập
+  const showHeader = location.pathname !== '/auth/sign-up' && location.pathname !== '/auth/sign-in'&& location.pathname !== '/auth/forgot-password' ;
 
   return (
     <>
@@ -39,10 +37,9 @@ const App = () => {
         <Route path="/account-management" element={<MyAccount />} />
         <Route path="/auth/sign-in" element={<SignInForm />} />
         <Route path="/auth/forgot-password" element={<ForgotPassword />} />
-        <Route path="/*" element={isAuthenticated ? <Home /> : <SignInForm />} />
+        <Route path="/" element={isAuthenticated ? <Home /> : <SignInForm />} />
         <Route path="/change-password" element={<ChangePassword />} />
         <Route path="/find-jobs" element={<FindJobs />} />
-        <Route path="/find-companies" element={<FindCompanies />} />
       </Routes>
     </>
   );
