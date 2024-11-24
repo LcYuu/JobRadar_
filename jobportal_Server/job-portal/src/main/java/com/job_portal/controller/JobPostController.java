7a566c6b325773c28f08ed45f830a80e931207ea
LcYuu <chindu1110@gmail.com>
2024-11-24 12:00:56 +0700

    Update

--- jobportal_Server/job-portal/src/main/java/com/job_portal/controller/JobPostController.java
+++ jobportal_Server/job-portal/src/main/java/com/job_portal/controller/JobPostController.java
@@ -478,7 +478,7 @@ public class JobPostController {
 	            pageable
 	    );
 	    return ResponseEntity.ok(jobs);
-
+	}
 	@GetMapping("/stats/daily")
 	public ResponseEntity<?> getDailyStats(
 	    @RequestParam String startDate,
