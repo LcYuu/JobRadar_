b98bb389dfc0cc67f352ea718859f9302d11f9b7
LcYuu <chindu1110@gmail.com>
2024-12-11 10:31:29 +0700

    Update notification

--- jobportal_Server/job-portal/src/main/java/com/job_portal/service/INotificationService.java
+++ jobportal_Server/job-portal/src/main/java/com/job_portal/service/INotificationService.java
@@ -7,7 +7,7 @@ import com.job_portal.enums.NotificationType;
 
 public interface INotificationService {
 	public boolean sendNotification(UUID userId, String title, String content, NotificationType type, String redirectUrl);
-	public boolean notifyNewJobPost(UUID companyId);
+	public boolean notifyNewJobPost(UUID companyId, UUID postId);
 	public boolean notifyApplicationReviewed(UUID seekerId, UUID postId, UUID companyId);
 	public boolean updateNotificationReadStatus(UUID notificationId);
 	public long countUnreadNotifications(UUID seekerId);
