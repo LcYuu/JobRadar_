184a52c859c25ed7852bc9e35def14fc802621db
DangGiaThuan <danggiathuanhl@gmail.com>
2025-03-07 14:06:24 +0700

    Block company

--- jobportal_Server/job-portal/src/main/java/com/job_portal/utils/OtpUtil.java
+++ jobportal_Server/job-portal/src/main/java/com/job_portal/utils/OtpUtil.java
@@ -7,14 +7,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class OtpUtil {
 	public String generateOtp() {
-//		Random random = new Random();
-//		int randomNum = random.nextInt(999999);
-//		String output = Integer.toString(randomNum);
-//		while(output.length() < 6) {
-//			output = "0" + output;
-//		}
-//		return output;
-		return "123456";
+		Random random = new Random();
+		int randomNum = random.nextInt(999999);
+		String output = Integer.toString(randomNum);
+		while(output.length() < 6) {
+			output = "0" + output;
+		}
+		return output;
 	}
 
 
